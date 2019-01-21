579cbe319bff242f156322d44ecb8d6ce745d1f0
nupsco <42444571+nupsco@users.noreply.github.com>
2019-01-21 13:46:19 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import os , time
-files   = ["ascii1.txt", "ascii2.txt", "ascii3.txt", "ascii4.txt", "ascii5.txt", "ascii6.txt", "ascii7.txt", "ascii8.txt", "ascii9.txt", "ascii10.txt"]
+files   = ["data/ascii1.txt", "data/ascii2.txt", "data/ascii3.txt", "data/ascii4.txt", "data/ascii5.txt", "data/ascii6.txt", "data/ascii7.txt", "data/ascii8.txt", "data/ascii9.txt", "data/ascii10.txt"]
 frames  = []
 
-os.system('cls') 
-#os.system('clear) for linux
+os.system('cls')
 for name in files:
     with open(name, "r", encoding="utf8") as f:
         frames.append(f.readlines())
 for i in range(1):
     for frame in frames:
         print("".join(frame))
-        time.sleep(1)
+        time.sleep(0.5)
         os.system('cls')
